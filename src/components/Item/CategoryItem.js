1a4ef19e93e22d3dc39503e3e964d0f366c7be0f
vnduy01 <duynt@sphinx-software.com>
2019-07-11 18:37:58 +0700

    map category item

--- src/components/Item/CategoryItem.js
+++ src/components/Item/CategoryItem.js
@@ -3,36 +3,37 @@ import propTypes from 'prop-types'
 
 export default class CategoryItem extends Component {
     render() {
-    
+
+        const {category} = this.props;
+
         return (
-            <div className="row margin-bottom-50">
-                <div className="col-md-4 md-margin-bottom-30">
+
+            <div className="col-md-4 md-margin-bottom-30">
                     <div className="overflow-h">
                         <a className="illustration-v3 illustration-img1" href="#">
                             <span className="illustration-bg">
                                 <span className="illustration-ads">
                                 <span className="illustration-v3-category">
-                                    <span className="product-category">Men</span>
-                                    <span className="product-amount">56 Items</span>
+                                    <span className="product-category">{category.name}</span>
+                                    <span className="product-amount">{category.amount}</span>
                                 </span>
                                 </span>
                             </span>
                         </a>
                     </div>
-                </div>     
-            </div>
+                </div> 
+        
         );
     }
 }
 
 CategoryItem.propTypes = {
-    category: propTypes.array
+    category: propTypes.object
 }
 
 CategoryItem.defaultProps = {
-    category: [
-        {
-        
-        }
-    ]
+    category: {
+        name: "Men",
+        amount: "56 Items"
+    }
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,49 +1,73 @@
-import React, {Component} from 'react';
-import { BrowserRouter,Route,Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import CollectionBanner from '../components/Banner/CollectionBanner';
-import Slider           from '../components/Slider/Slider';
-import SaleBanner       from '../components/Banner/SaleBanner';
-import ProductItem      from '../components/Item/ProductItem';
-import CategoryItem     from '../components/Item/CategoryItem';
-import TwitterBlock     from '../components/TwitterBlock/TwitterBlock';
-import ProductService   from '../components/ProductService/ProductService';
-import Illustrationv5   from '../components/Illustrationv5/Illustrationv5';
-import Sponsors         from '../components/Sponsors/Sponsors';
+import Slider from '../components/Slider/Slider';
+import SaleBanner from '../components/Banner/SaleBanner';
+import ProductItem from '../components/Item/ProductItem';
+import CategoryItem from '../components/Item/CategoryItem';
+import TwitterBlock from '../components/TwitterBlock/TwitterBlock';
+import ProductService from '../components/ProductService/ProductService';
+import Illustrationv5 from '../components/Illustrationv5/Illustrationv5';
+import Sponsors from '../components/Sponsors/Sponsors';
 
 
 export default class Home extends Component {
-    
+
     render() {
 
-        return(
+        const categories = [
+            {
+                name: "Men",
+                amount: "450"
+            },
+            {
+                name: "Men1",
+                amount: "450"
+            },
+            {
+                name: "Men2",
+                amount: "450"
+            }
+        ];
+
+        return (
             <BrowserRouter>
                 <div>
-               <div className="wrapper">
-                   
-                   <Slider/>
-                   <div className="container content-md">
-                       <SaleBanner/>
-                       <div className="heading heading-v1 margin-bottom-20">
-                           <h2>Featured products</h2>
-                           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio elit, ultrices vel cursus sed, placerat ut leo. Phasellus in magna erat. Etiam gravida convallis augue non tincidunt. Nunc lobortis dapibus neque quis lacinia. Nam dapibus tellus sit amet odio venenatis</p>
-                       </div>    
-                            <ProductItem/>
-                            <CategoryItem/>
-                       <div className="heading heading-v1 margin-bottom-40">
-                           <h2>Latest products</h2>
-                       </div>
-                            <ProductItem/>
-                   </div>
-                        <TwitterBlock/>
-                   <div className="container">
-                        <ProductService/>
-                        <Illustrationv5/>
-                   </div>
-                        <CollectionBanner/>
-                        <Sponsors/>
-                        
-               </div>
-           </div>
+                    <div className="wrapper">
+
+                        <Slider />
+                        <div className="container content-md">
+                            <SaleBanner />
+                            <div className="heading heading-v1 ymargin-bottom-20">
+                                <h2>Featured products</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio elit, ultrices vel cursus sed, placerat ut leo. Phasellus in magna erat. Etiam gravida convallis augue non tincidunt. Nunc lobortis dapibus neque quis lacinia. Nam dapibus tellus sit amet odio venenatis</p>
+                            </div>
+                            <ProductItem />
+                            <div className="row margin-bottom-50">
+                                {categories.map((category, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <CategoryItem category={category} />
+                                        </div>
+                                    )
+                                })}
+                            </div>
+
+                            <div className="heading heading-v1 margin-bottom-40">
+                                <h2>Latest products</h2>
+                            </div>
+                            <ProductItem />
+                        </div>
+                        <TwitterBlock />
+                        <div className="container">
+                            <ProductService />
+                            <Illustrationv5 />
+                        </div>
+                        <CollectionBanner />
+                        <Sponsors />
+
+                    </div>
+                </div>
             </BrowserRouter>
         )
     }
